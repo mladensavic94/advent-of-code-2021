7134c508d04c1a75810fe931c07d27ca2014da48
msavic <mladen.savic@igt.com>
2021-12-03 16:30:41 +0100

    day 3 complete

--- src/main/scala/Day3.scala
+++ src/main/scala/Day3.scala
@@ -28,14 +28,40 @@ object Day3 {
     for (sum <- digits) {
       counters = (counters, sum).zipped.map(_ + _)
     }
-    for (i <- counters.indices) {
-      val temp = if (counters(i) >= 500)
-        digits.filter(l => l(i) == 1)
+    println(counters)
+    var mostCommonBits = List.from(digits)
+    var counter = 0
+    while (mostCommonBits.length > 1) {
+      mostCommonBits = if (counters(counter) >= mostCommonBits.length / 2.0)
+        mostCommonBits.filter(l => l(counter) == 1)
       else
-        digits.filter(l => l(i) == 0)
-      if(temp.length == 1)
-        println(temp.head)
+        mostCommonBits.filter(l => l(counter) == 0)
+      counter += 1
+      counters = List.fill(12)(0)
+      for (sum <- mostCommonBits) {
+        counters = (counters, sum).zipped.map(_ + _)
+      }
+      println(mostCommonBits)
+      println(counters)
     }
+    var counters2 = List.fill(12)(0)
+    for (sum <- digits) {
+      counters2 = (counters2, sum).zipped.map(_ + _)
+    }
+    var leastCommonBits = List.from(digits)
+    var counter2 = 0
+    while (leastCommonBits.length > 1) {
+      leastCommonBits = if (counters2(counter2) >= leastCommonBits.length / 2.0)
+        leastCommonBits.filter(l => l(counter2) == 0)
+      else
+        leastCommonBits.filter(l => l(counter2) == 1)
+      counter2 += 1
+      counters2 = List.fill(12)(0)
+      for (sum <- leastCommonBits) {
+        counters2 = (counters2, sum).zipped.map(_ + _)
+      }
+    }
+    println(Integer.parseInt(leastCommonBits.head.mkString(""), 2) * Integer.parseInt(mostCommonBits.head.mkString(""), 2))
 
   }
 }
